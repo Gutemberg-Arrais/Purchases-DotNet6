a2c72ed0acaa41a7bcd7fbef84af767de6dc9510
Gutemberg-Arrais <arraisgutemberg@gmail.com>
2023-02-03 12:30:11 -0300

    Adicionado respository

---
+++ ApiDotNet6.infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ApiDotNet6.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApiDotNet6.infra.Data.Maps
+{
+    public class PersonMap : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.ToTable("Person");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .HasColumnName("id_person")
+                .UseIdentityColumn();
+
+            builder.Property(c => c.Document).HasColumnName("document");
+
+            builder.Property(c => c.Name).HasColumnName("name");
+
+            builder.Property(c => c.Phone).HasColumnName("phone");
+
+            builder.HasMany(c => c.Purchases).WithOne(p => p.Person).HasForeignKey(c => c.PersonId);
+        }
+    }
+}
+
---
+++ ApiDotNet6.infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ApiDotNet6.Domain.Entities;
+using ApiDotNet6.Domain.Repositories;
+using ApiDotNet6.infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiDotNet6.infra.Data.Repositories
+{
+	public class ProductRepository : IProductRepository
+	{
+        private readonly ApplicationDbContext _db;
+
+		public ProductRepository(ApplicationDbContext db) 
+		{
+            _db = db;
+		}
+
+        public async Task<Product> CreateProductAsync(Product product)
+        {
+            _db.Add(product);
+            await _db.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task DeleteAsync(Product product)
+        {
+            _db.Remove(product);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task EditAsync(Product product)
+        {
+            _db.Update(product);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _db.Products.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<ICollection<Product>> GetProductsAsync()
+        {
+            return await _db.Products.ToListAsync();
+        }
+    }
+}
+
